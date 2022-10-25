625f93f5cf622c5eac36b4b85644bd0fde5b4a5b
bsmuse <bsmuse@tutamail.com>
2022-10-25 08:33:20 -0600

    App base complete. Add audio and message at end to be fully done

--- dog.js
+++ dog.js
@@ -1,9 +1,16 @@
-// Create the Dog class here
 class Dog {
     constructor(data) {
         Object.assign(this,data)
-
     }
     getDogHtml() {
-        
+        const { name, avatar, age, bio, hasBeenLiked, hasBeenSwiped } = this
+            return `
+            <img id="dog" src="${avatar}">
+            <div class="details">
+                <p id="name">${name}, ${age}</p>
+                <p id="tagline">${bio}</p>
+            </div>`
+    }
 }
+
+export default Dog
--- module.js
+++ module.js
@@ -1,1 +1,38 @@
-// Remember to import the data and Dog class!
+import Dog from '/dog.js' 
+import dogs from '/data.js'
+
+let dogsArray = dogs
+console.log(dogs.length)
+
+const getNewDog = () => {
+    const nextDogData = dogsArray.shift()
+    return nextDogData ? new Dog(nextDogData) : {}
+}
+
+let suitor = getNewDog()
+
+document.getElementById("like-btn").addEventListener('click', () => {
+    console.log('clicked')
+    suitor.hasBeenLiked = true
+    console.log(suitor.hasBeenLiked)
+    suitor = getNewDog()
+    document.getElementById('liked').style.visibility= 'visible'
+    setTimeout(() => {render()},1500)
+})
+
+document.getElementById("dislike-btn").addEventListener('click', () => {
+    console.log('clicked')
+    suitor.hasBeenSwiped = true
+    console.log(suitor.hasBeenSwiped)
+    suitor = getNewDog()
+    document.getElementById('disliked').style.visibility= 'visible'
+    setTimeout(() => {render()},2000)
+})
+
+const render = () => {
+    document.getElementById('liked').style.visibility= 'hidden'
+    document.getElementById('disliked').style.visibility= 'hidden'
+    document.querySelector(".contain-data").innerHTML = suitor.getDogHtml()
+}
+
+render()
