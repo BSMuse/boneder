8dfdbb34d9663a1a533c9b77fcd97d7fc76f4366
bsmuse <bsmuse@tutamail.com>
2022-10-25 20:55:22 -0600

    Project fully finished

--- module.js
+++ module.js
@@ -1,38 +1,56 @@
 import Dog from '/dog.js' 
 import dogs from '/data.js'
 
-let dogsArray = dogs
-console.log(dogs.length)
+const likeBtn = document.getElementById("like-btn")
+const dislikeBtn = document.getElementById('dislike-btn')
 
 const getNewDog = () => {
-    const nextDogData = dogsArray.shift()
+    const nextDogData = dogs.shift()
     return nextDogData ? new Dog(nextDogData) : {}
 }
 
 let suitor = getNewDog()
 
-document.getElementById("like-btn").addEventListener('click', () => {
-    console.log('clicked')
+likeBtn.addEventListener('click', () => {
+    likeBtn.setAttribute('disabled','')
+    dislikeBtn.setAttribute('disabled','')
     suitor.hasBeenLiked = true
     console.log(suitor.hasBeenLiked)
     suitor = getNewDog()
     document.getElementById('liked').style.visibility= 'visible'
-    setTimeout(() => {render()},1500)
+    new Audio("/bowwow.mp3").play()
+    setTimeout(() => {
+        likeBtn.removeAttribute('disabled','')
+        dislikeBtn.removeAttribute('disabled','')
+        render()
+    },1500)
 })
 
-document.getElementById("dislike-btn").addEventListener('click', () => {
-    console.log('clicked')
+dislikeBtn.addEventListener('click', () => {
+    dislikeBtn.setAttribute('disabled','')
+    likeBtn.setAttribute('disabled','')
     suitor.hasBeenSwiped = true
     console.log(suitor.hasBeenSwiped)
     suitor = getNewDog()
     document.getElementById('disliked').style.visibility= 'visible'
-    setTimeout(() => {render()},2000)
+    new Audio("/bark.mp3").play()
+    setTimeout(() => {
+        likeBtn.removeAttribute('disabled','')
+        dislikeBtn.removeAttribute('disabled','')
+        render()
+    },1500)
 })
 
 const render = () => {
     document.getElementById('liked').style.visibility= 'hidden'
     document.getElementById('disliked').style.visibility= 'hidden'
-    document.querySelector(".contain-data").innerHTML = suitor.getDogHtml()
+    !suitor.getDogHtml ? document.querySelector("main").innerHTML = `
+        <div class = "end-text">
+        <h1>Looks like we're out of dogs for you to bone!</h1>
+        <i class="fa-solid fa-dog"></i>
+        <p>Come back later to see what we can scrounge from the pound...</p>
+        </div>` 
+    :  document.querySelector(".contain-data").innerHTML = suitor.getDogHtml()
 }
 
 render()
