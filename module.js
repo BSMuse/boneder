9a114b6e2acd524e873bd9c83302a10e49a14608
bsmuse <bsmuse@tutamail.com>
2022-10-26 07:32:31 -0600

    Update JS to be more concise and add comments

--- module.js
+++ module.js
@@ -4,6 +4,9 @@ import dogs from '/data.js'
 const likeBtn = document.getElementById("like-btn")
 const dislikeBtn = document.getElementById('dislike-btn')
 
+// this function allows each of the buttons to shift through the objects in data's array
+// ternary operator will return the next dog object through the Dog class if there is data left in the object after shift
+// otherwise, it returns an empty object
 const getNewDog = () => {
     const nextDogData = dogs.shift()
     return nextDogData ? new Dog(nextDogData) : {}
@@ -11,13 +14,17 @@ const getNewDog = () => {
 
 let suitor = getNewDog()
 
-likeBtn.addEventListener('click', () => {
+// this function disables buttons and call's the suitor variable to access the getNewDog function to shift to a new dog
+const dogTransition = () => {
     likeBtn.setAttribute('disabled','')
     dislikeBtn.setAttribute('disabled','')
-    suitor.hasBeenLiked = true
-    console.log(suitor.hasBeenLiked)
     suitor = getNewDog()
-    document.getElementById('liked').style.visibility= 'visible'
+}
+
+likeBtn.addEventListener('click', () => {
+    dogTransition()
+    suitor.hasBeenLiked = true
+    document.getElementById('liked').style.visibility = 'visible'
     new Audio("/bowwow.mp3").play()
     setTimeout(() => {
         likeBtn.removeAttribute('disabled','')
@@ -27,12 +34,9 @@ likeBtn.addEventListener('click', () => {
 })
 
 dislikeBtn.addEventListener('click', () => {
-    dislikeBtn.setAttribute('disabled','')
-    likeBtn.setAttribute('disabled','')
+    dogTransition()
     suitor.hasBeenSwiped = true
-    console.log(suitor.hasBeenSwiped)
-    suitor = getNewDog()
-    document.getElementById('disliked').style.visibility= 'visible'
+    document.getElementById('disliked').style.visibility = 'visible'
     new Audio("/bark.mp3").play()
     setTimeout(() => {
         likeBtn.removeAttribute('disabled','')
@@ -41,9 +45,11 @@ dislikeBtn.addEventListener('click', () => {
     },1500)
 })
 
+// render function allows the "stamps" to disappear and set new dog data to method in dog.js 
+// ternary operator will detect if there is no new dog data to post, and if so, post the html below
 const render = () => {
-    document.getElementById('liked').style.visibility= 'hidden'
-    document.getElementById('disliked').style.visibility= 'hidden'
+    document.getElementById('liked').style.visibility = 'hidden'
+    document.getElementById('disliked').style.visibility = 'hidden'
     !suitor.getDogHtml ? document.querySelector("main").innerHTML = `
         <div class = "end-text">
         <h1>Looks like we're out of dogs for you to bone!</h1>
