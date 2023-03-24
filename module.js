ed9a33c899fd0cbbf3ba1f3d42a249c7c19a2347
bsmuse <bsmuse@tutamail.com>
2023-03-24 16:24:12 -0600

    commit

--- module.js
+++ module.js
@@ -60,19 +60,20 @@ dislikeBtn.addEventListener("click", async () => {
 
 // render function allows the "stamps" to disappear and set new dog data to method in dog.js
 const render = async () => {
-//   if (!suitor) {
-//     document.querySelector(".contain-data").innerHTML = `
-//       <div class="no-data">
-//         <p>No more dogs to show!</p>
-//       </div>
-//     `;
-//     return;
-//   }
-  document.getElementById("liked").style.visibility = "hidden";
-  document.getElementById("disliked").style.visibility = "hidden";
-  document.querySelector(".contain-data").innerHTML = await suitor.getDogHtml();
-};
-
+    if (!suitor) {
+      document.querySelector(".contain-data").innerHTML = `
+        <div class="no-data">
+          <p>No more dogs to show!</p>
+        </div>
+      `;
+      return;
+    }
+    document.getElementById("liked").style.visibility = "hidden";
+    document.getElementById("disliked").style.visibility = "hidden";
+    const dogHtml = suitor.getDogHtml ? await suitor.getDogHtml() : '';
+    document.querySelector(".contain-data").innerHTML = dogHtml;
+  };
+  
 // initial render
 dogTransition();
 render();
