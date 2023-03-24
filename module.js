0cca3902b33d4bb0f67eebff0f594651ca9826dc
bsmuse <bsmuse@tutamail.com>
2023-03-24 14:51:27 -0600

    commit

--- module.js
+++ module.js
@@ -24,11 +24,11 @@ const dogTransition = () => {
 likeBtn.addEventListener('click', async () => {
     fetch("https://randomuser.me/api/")
     .then(res => res.json())
-    .then(data => console.log(data[0].results.name.first))
+    .then(data => console.log(data[0])
 
     fetch("https://randomuser.me/api/")
     .then(res => res.json())
-    .then(data => console.log(data[0].results.dob.age))
+    .then(data => console.log(data[0])
 
     fetch("https://dog.ceo/api/breeds/image/random")
     .then(res => res.json())
