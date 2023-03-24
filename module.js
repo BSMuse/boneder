1ab89e9c6b504911a01391c63c5f3b133b9eca15
bsmuse <bsmuse@tutamail.com>
2023-03-24 16:39:09 -0600

    commit

--- module.js
+++ module.js
@@ -1,19 +1,45 @@
-import Dog from '/dog.js'
+// import Dog from '/dog.js'
 
 const likeBtn = document.getElementById("like-btn");
 const dislikeBtn = document.getElementById("dislike-btn");
 
+class Dog {
+  constructor(data) {
+    Object.assign(this, data);
+  }
+
+  getDogHtml() {
+    const { name, avatar, age, bio } = this;
+
+    return `
+      <img id="dog" src="${avatar}">
+      <div class="details">
+        <p id="name">${name}, ${age}</p>
+        <p id="tagline">${bio}</p>
+      </div>
+    `;
+  }
+}
+
+const fetchJson = async (url) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(`Failed to fetch ${url}: ${error}`);
+    return null;
+  }
+};
+
 const getNewDog = async () => {
-  const response = await fetch('https://randomuser.me/api/');
-  const userData = await response.json();
-  const dogResponse = await fetch('https://dog.ceo/api/breeds/image/random');
-  const dogData = await dogResponse.json();
-  const jokeResponse = await fetch('https://icanhazdadjoke.com', {
-    headers: {
-      Accept: 'application/json',
-    },
-  });
-  const jokeData = await jokeResponse.json();
+  const userData = await fetchJson("https://randomuser.me/api/");
+  const dogData = await fetchJson("https://dog.ceo/api/breeds/image/random");
+  const jokeData = await fetchJson("https://icanhazdadjoke.com");
+
+  if (!userData || !dogData || !jokeData) {
+    return null;
+  }
 
   return new Dog({
     name: userData.results[0].name.first,
@@ -27,54 +53,57 @@ const getNewDog = async () => {
 
 let suitor = null;
 
-// this function disables buttons and call's the suitor variable to access the getNewDog function to shift to a new dog
-const dogTransition = async () => {
+const disableButtons = () => {
   likeBtn.setAttribute("disabled", "");
   dislikeBtn.setAttribute("disabled", "");
+};
+
+const enableButtons = () => {
+  likeBtn.removeAttribute("disabled");
+  dislikeBtn.removeAttribute("disabled");
+};
+
+const dogTransition = async () => {
+  disableButtons();
   suitor = await getNewDog();
+  enableButtons();
 };
 
 likeBtn.addEventListener("click", async () => {
-  await dogTransition(); // Wait for the dogTransition function to complete before proceeding
+  await dogTransition();
   suitor.hasBeenLiked = true;
   document.getElementById("liked").style.visibility = "visible";
   new Audio("/bowwow.mp3").play();
   setTimeout(() => {
-    likeBtn.removeAttribute("disabled");
-    dislikeBtn.removeAttribute("disabled");
     render();
   }, 1500);
 });
 
 dislikeBtn.addEventListener("click", async () => {
-  await dogTransition(); // Wait for the dogTransition function to complete before proceeding
+  await dogTransition();
   suitor.hasBeenSwiped = true;
   document.getElementById("disliked").style.visibility = "visible";
   new Audio("/bark.mp3").play();
   setTimeout(() => {
-    likeBtn.removeAttribute("disabled");
-    dislikeBtn.removeAttribute("disabled");
     render();
   }, 1500);
 });
 
-// render function allows the "stamps" to disappear and set new dog data to method in dog.js
 const render = async () => {
-    console.log(suitor)
-    if (!suitor) {
-      document.querySelector(".contain-data").innerHTML = `
-        <div class="no-data">
-          <p>No more dogs to show!</p>
-        </div>
-      `;
-      return;
-    }
-    document.getElementById("liked").style.visibility = "hidden";
-    document.getElementById("disliked").style.visibility = "hidden";
-    const dogHtml = suitor.getDogHtml ? await suitor.getDogHtml() : '';
-    document.querySelector(".contain-data").innerHTML = dogHtml;
-  };
-  
-// initial render
+  if (!suitor) {
+    document.querySelector(".contain-data").innerHTML = `
+      <div class="no-data">
+        <p>No more dogs to show!</p>
+      </div>
+    `;
+    return;
+  }
+
+  document.getElementById("liked").style.visibility = "hidden";
+  document.getElementById("disliked").style.visibility = "hidden";
+  const dogHtml = await suitor.getDogHtml();
+  document.querySelector(".contain-data").innerHTML = dogHtml;
+};
+
 dogTransition();
 render();
